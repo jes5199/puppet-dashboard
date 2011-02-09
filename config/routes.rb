18665770f098367b12754af2bc997793969e5b3f
Jacob Helwig <jacob@puppetlabs.com>
2011-02-09 13:29:52 -0800

    Revert "wip: Changed config/routes.rB"
    This reverts commit 75483e304e3d6b8cbec8c0173ef00f1b497c3689.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,38 @@
-(in /Users/max/Documents/Workspace/puppet-dashboard)
-PuppetDashboard::Application.routes.draw do
+ActionController::Routing::Routes.draw do |map|
+  map.resources :node_classes, :collection => {:search => :get} do |classes|
+    classes.resources :nodes, :requirements => {:id => /.*/}
+  end
 
+  map.resources :node_groups,
+    :member     => { :diff  => :get },
+    :collection => {:search => :get } do |groups|
+      groups.resources :nodes, :requirements => {:id => /.*/}
+    end
+
+  map.resources :nodes,
+    :member => {
+      :hide => :put,
+      :unhide => :put,
+      :facts => :get,
+      :reports => :get},
+    :collection => {
+     :unreported => :get,
+     :no_longer_reporting => :get,
+     :hidden => :get,
+     :search => :get},
+    :requirements => {:id => /[^\/]+/}
+
+  map.resources :reports,
+    :collection => {
+      :search => :get,
+    }
+
+  map.upload "reports/upload", :controller => :reports, :action => "upload", :conditions => { :method => :post }
+
+  map.release_notes '/release_notes', :controller => :pages, :action => :release_notes
+
+  map.root :controller => :pages, :action => :home
+
+  map.connect ':controller/:action/:id'
+  map.connect ':controller/:action/:id.:format'
 end
