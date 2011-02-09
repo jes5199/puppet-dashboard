76de2a020723c8b9d76626e52089b37f3c0ec1a3
Jesse Wolfe <jes5199@gmail.com>
2011-02-09 13:25:33 -0800

    wip: new files, oops

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PuppetDashboard::Application.config.secret_token = 'ffb64ed8260d60108e68da11a8dae07342c3a88a5d5d4f92f035ea23c13a539d4e4ce47086fa8f7d50e2e4c095c400cf0301ed4ac01125afa5a7ec50b924616e'
