63a6d6e1f1da0c57a15166e6158b071688a47344
Jesse Wolfe <jes5199@gmail.com>
2011-02-09 14:32:41 -0800

    wip: remove deprecated const from config

--- config/initializers/check_migration_version.rb
+++ config/initializers/check_migration_version.rb
@@ -3,7 +3,7 @@
 # skip when run from tasks like rake db:migrate, or from Capistrano rules
 # that need to run before the migration rule
 current_version = ActiveRecord::Migrator.current_version rescue 0
-highest_version = Dir.glob("#{RAILS_ROOT}/db/migrate/*.rb" ).map { |f|
+highest_version = Dir.glob("#{Rails.root}/db/migrate/*.rb" ).map { |f|
   f.match(/(\d+)_.*\.rb$/) ? $1.to_i : 0
 }.max
 
@@ -11,5 +11,5 @@ abort "Database isn't the current migration version: " \
       "expected #{highest_version}, got #{current_version}\n" \
       "You must either run 'rake db:migrate' or set environmental variable NO_MIGRATION_CHECK" \
   unless current_version == highest_version or \
-  defined?(Rake) or ENV['NO_MIGRATION_CHECK'] 
+  defined?(Rake) or ENV['NO_MIGRATION_CHECK']
 
